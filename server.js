eddc11fd3564f1ee8feb3a548d8bc30f1dbf35e6
SihanCai <cai_s3@denison.edu>
2021-05-13 22:54:56 -0500

    Change

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const jsonServer = require('json-server')
+const serverless = require("serverless-http");
 const path = require('path')
 const express = require('express')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-const root = __dirname + '/build'
-server.use(express.static(root, { maxAge: 86400000 }))
+// const root = __dirname + '/build'
+// server.use(express.static(root, { maxAge: 86400000 }))
 server.use(middlewares)
-const reactRouterWhiteList = ['/create', '/edit/:itemId']
-server.get(reactRouterWhiteList, function (request, response) {
-  response.sendFile(path.resolve(root, 'index.html'))
-})
+// const reactRouterWhiteList = ['/create', '/edit/:itemId']
+// server.get(reactRouterWhiteList, function (request, response) {
+//   response.sendFile(path.resolve(root, 'index.html'))
+// })
+
+
 
 server.use(router)
 server.listen(3000, () => {
   console.log('JSON Server is running')
 })
+
+module.exports = app;
+module.exports.handler = serverless(app);
